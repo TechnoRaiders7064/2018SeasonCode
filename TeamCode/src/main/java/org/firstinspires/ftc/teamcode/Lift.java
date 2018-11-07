0a475cc9bc571346a0f2450b8894aae6a4c7c540
Jaden Crow <crowjaden@gmail.com>
2018-11-07 14:26:14 -0600

    add servo to lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -10,18 +11,19 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Lift {
     private DcMotor leftLiftMotor = null;
     private DcMotor rightLiftMotor = null;
-    private Servo
+    private Servo clampServo = null;
 
     private final int LIFTMAX = 1000;
 
 
-    Lift(DcMotor leftLiftMotor, DcMotor rightLiftMotor)
+    Lift(DcMotor leftLiftMotor, DcMotor rightLiftMotor, Servo clampServo)
     {
         this.leftLiftMotor = leftLiftMotor;
         this.rightLiftMotor = rightLiftMotor;
+        this.clampServo = clampServo;
+
         this.leftLiftMotor.setDirection(DcMotor.Direction.FORWARD);
         this.rightLiftMotor.setDirection(DcMotor.Direction.REVERSE);
-
     }
 
     public void update(Gamepad gamepad, Telemetry telemetry)
@@ -29,6 +31,14 @@ public class Lift {
         int leftPosition = leftLiftMotor.getCurrentPosition();
         int rightPosition = rightLiftMotor.getCurrentPosition();
 
+        if(gamepad.a)
+        {
+            clampServo.setPosition(90);
+        }
+        else
+        {
+            clampServo.setPosition(0);
+        }
         //&& (leftPosition <= LIFTMAX && rightPosition <= LIFTMAX)
         if(gamepad.dpad_up )
         {
