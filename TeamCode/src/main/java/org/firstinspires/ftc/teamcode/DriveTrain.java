6f6393f61b1454b884b9812ef8aa266bc3699b6d
Jaden Crow <crowjaden@gmail.com>
2018-11-08 18:44:36 -0600

    add DriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class DriveTrain {
+    DcMotor frontLeftDrive = null;
+    DcMotor frontRightDrive = null;
+    DcMotor backLeftDrive = null;
+    DcMotor backRightDrive = null;
+
+    DriveTrain(DcMotor frontLeftDrive, DcMotor frontRightDrive, DcMotor backLeftDrive, DcMotor backRightDrive)
+    {
+        this.frontLeftDrive =  frontLeftDrive;
+        this.frontRightDrive = frontRightDrive;
+        this.backLeftDrive = backLeftDrive;
+        this.backRightDrive = backRightDrive;
+
+        this.frontLeftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        this.backLeftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        this.frontRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.backRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void update(Gamepad gamepad, Telemetry telemetry)
+    {
+        double drive = -gamepad.left_stick_y;
+        double turn = gamepad.left_stick_x;
+        double strafe = gamepad.right_stick_x;
+
+        frontLeftDrive.setPower(Range.clip(drive + turn - strafe, -1.0, 1.0) );
+        frontRightDrive.setPower(Range.clip(drive - turn + strafe, -1.0, 1.0) );
+        backLeftDrive.setPower(Range.clip(drive + turn + strafe, -1.0, 1.0) );
+        backRightDrive.setPower(Range.clip(drive - turn - strafe, -1.0, 1.0) );
+    }
+
+
+
 }
